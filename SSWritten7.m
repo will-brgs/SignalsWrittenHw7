eafb14db4a28c19c54a0cd3aced6d3463cc9ae6c
Will <Will@Will-laptop>
2024-04-13 13:11:35 -0500

    bawling

--- SSWritten7.m
+++ SSWritten7.m
@@ -3,11 +3,59 @@
 % * Author:                   Will Burgess
 % * Class:                    ESE 351
 % * Date:                     Created 4/12/2024, Last Edited 4/16/2024
+%% Variable Initialization
 
-%% 1.iv.i
+close all 
+clc
 
-%% 1.iv.ii
+%% 1.iv.i : Lowpass RC LCCDE
 
-%% 1.iv.iii
+%Lowpass
+tau = 0.01; 
+samplePeriod = 0.1; 
 
-%% 1.iv.iv
+a = [1,1/tau];
+b = 1/tau;
+
+sys = tf(b, a, samplePeriod);
+% plot
+figure, hold on
+bode(sys);
+title('Bode plot for RC LoPass Coefficients')
+
+hold off
+%% 1.iv.ii : HiPass RC LCCDE 
+%Hipass
+tau = 0.01; 
+samplePeriod = 0.1; 
+
+a = [1, 1/tau] ;
+b = [1,0];
+
+sys = tf(b, a, samplePeriod);
+% plot
+figure, hold on
+bode(sys);
+title('Bode plot for RC HiPass Coefficients')
+hold off
+
+%% 1.iv.iii: Mass spring damper system
+a = [1, 2*0.5*2, 2^2];
+b = 1;
+
+sys = tf(b, a);
+
+%Plot
+figure, hold on
+bode(sys);
+title('Bode plot for Mass Spring Damper System');
+hold off
+%% 1.iv.iv : 2nd order system with 2 zeros
+
+sys = tf([1,2,1], [1,20,100]);
+
+%Plot
+figure, hold on
+bode(sys);
+title('Bode plot of 2nd order system with 2 Zeros');
+hold off
